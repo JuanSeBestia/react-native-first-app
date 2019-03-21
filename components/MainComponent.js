651538b4055424aef1a0a5a252b4e42bd68b559b
JuanSe Dussan <juandussan@seven4n.com>
2019-03-21 10:36:34 -0500

    Test Network

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
+import { View, Platform, Text, ScrollView, Image, StyleSheet, NetInfo, ToastAndroid } from 'react-native';
 import { createStackNavigator, createAppContainer, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import DishDetail from './DIshDetailComponent';
 // in managed apps:
@@ -169,8 +169,39 @@ const MainContainer = createAppContainer(MainNavigator);
 
 class Main extends Component {
 
+    componentWillUnmount() {
+        NetInfo.removeEventListener('connectionChange', this.handleConnectivityChange);
+    }
+
+    handleConnectivityChange = (connectionInfo) => {
+        switch (connectionInfo.type) {
+            case 'none':
+                ToastAndroid.show('You are now offline!', ToastAndroid.LONG);
+                break;
+            case 'wifi':
+                ToastAndroid.show('You are now connected to WiFi!', ToastAndroid.LONG);
+                break;
+            case 'cellular':
+                ToastAndroid.show('You are now connected to Cellular!', ToastAndroid.LONG);
+                break;
+            case 'unknown':
+                ToastAndroid.show('You now have unknown connection!', ToastAndroid.LONG);
+                break;
+            default:
+                break;
+        }
+    }
     componentDidMount() {
         console.log("Main:componentDidMount:props", this.props);
+
+        NetInfo.getConnectionInfo()
+            .then((connectionInfo) => {
+                ToastAndroid.show('Initial Network Connectivity Type: '
+                    + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType,
+                    ToastAndroid.LONG)
+            });
+
+        NetInfo.addEventListener('connectionChange', this.handleConnectivityChange);
 
         if (!this.props.dishes.dishes.length) this.props.fetchDishes();
         else console.log("Main:componentDidMount:Dishes already loaded");
